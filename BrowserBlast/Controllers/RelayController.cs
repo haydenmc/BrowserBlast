e7c79ce309b05863da3d0fca452a46350f719754
Darren Cheng <zades@outlook.com>
2014-10-18 13:41:24 -0400

    Hadyen smells

---
+++ BrowserBlast/Controllers/RelayController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrowserBlast.Controllers
+{
+    public class RelayController : Controller
+    {
+        // GET: Relay
+        public ActionResult Index()
+        {
+			//bla bla bla hayden smells
+			
+            return View();
+        }
+    }
+}
