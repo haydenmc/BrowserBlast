a5c9ba02a92ca532e584e751dbda82d49c2fca0f
Hayden <hayden@outlook.com>
2014-10-18 04:15:01 -0400

    Added hub methods on the server-side for organizing connections and updating pointer positions.

--- BrowserBlast/Hubs/GameHub.cs
+++ BrowserBlast/Hubs/GameHub.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace BrowserBlast.Hubs
 {
 	public class GameHub : Hub
 	{
-		public void Hello()
+		private static List<string> ConnectionIds = new List<string>();
+		public override Task OnConnected()
 		{
-			Clients.All.hello();
+			ConnectionIds.Add(Context.ConnectionId);
+			Clients.AllExcept(Context.ConnectionId).addPointer(Context.ConnectionId);
+			return base.OnConnected();
+		}
+		public override Task OnDisconnected(bool stopCalled)
+		{
+			ConnectionIds.Remove(Context.ConnectionId);
+			Clients.All.removePointer(Context.ConnectionId);
+			return base.OnDisconnected(stopCalled);
+		}
+		public override Task OnReconnected()
+		{
+			if (!ConnectionIds.Contains(Context.ConnectionId))
+			{
+				ConnectionIds.Add(Context.ConnectionId);
+				Clients.AllExcept(Context.ConnectionId).addPointer(Context.ConnectionId);
+			}
+			return base.OnReconnected();
+		}
+		public void UpdatePointer(int newX, int newY)
+		{
+			Clients.AllExcept(Context.ConnectionId).updatePointer(Context.ConnectionId, newX, newY);
 		}
 	}
 }
