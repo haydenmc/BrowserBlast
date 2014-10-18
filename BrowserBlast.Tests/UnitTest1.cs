765715019b261666d42c54f50f52f4a89e78754e
lucaslou <lucaslou317@gmail.com>
2014-10-18 15:00:40 -0400

    relay and unit test

--- BrowserBlast.Tests/UnitTest1.cs
+++ BrowserBlast.Tests/UnitTest1.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowserBlast.Controllers;
+using System.Web;
+using HtmlAgilityPack;
 
-namespace BrowserBlast.Tests
+
+namespace BrowserBlast.Test
 {
 	[TestClass]
 	public class UnitTest1
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void TestURL()
 		{
+            System.Diagnostics.Debug.WriteLine("ppoop");
+            RelayController test = new RelayController();
+            //HtmlDocument poop = test.Index("www.google.com");
+            System.Diagnostics.Debug.WriteLine("???"+test.Index("www.google.com").ToString());
+            System.Diagnostics.Debug.WriteLine("ppoop1");
 		}
 	}
 }
--- BrowserBlast/Controllers/RelayController.cs
+++ BrowserBlast/Controllers/RelayController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HtmlAgilityPack;
+using System.Web.Http;
 
 namespace BrowserBlast.Controllers
 {
     public class RelayController : Controller
     {
         // GET: Relay
-        public ActionResult Index()
+        public ActionResult Index(String url)
         {
 			//bla bla bla hayden smells
-			
-            return View();
+            //TODO: check valid url
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(url);                       
+            return View(doc);
         }
     }
 }
