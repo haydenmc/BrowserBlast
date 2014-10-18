3f94ea15c16332848ec523a3f8a5ce3074235812
Hayden <hayden@outlook.com>
2014-10-18 16:38:22 -0400

    Update to relay - mostly working.

--- BrowserBlast/Controllers/RelayController.cs
+++ BrowserBlast/Controllers/RelayController.cs
@@ -17,11 +17,35 @@ namespace BrowserBlast.Controllers
             //TODO: check valid url
 			HtmlWeb web = new HtmlWeb();
 			HtmlDocument doc = web.Load("http://" + url);
-			var headNode = doc.DocumentNode.SelectSingleNode("//head");
+			var root = doc.DocumentNode;
+
+			// Kill all of the JS
+			var jsNodes = root.SelectNodes("//script");
+			foreach (var jsNode in jsNodes)
+			{
+				jsNode.Remove();
+			}
+
+			// Inject JS into the head node
+			var headNode = root.SelectSingleNode("//head");
 			var scriptNode = doc.CreateElement("script");
 			scriptNode.Attributes.Add("type", "text/javascript");
 			scriptNode.InnerHtml = "alert('shittles');";
 			headNode.AppendChild(scriptNode);
+
+			// Replace paths of all images to be absolute.
+			var imageNodes = root.SelectNodes("//img");
+			foreach (var imageNode in imageNodes)
+			{
+				if (imageNode.Attributes.Contains("src") && 
+					!imageNode.Attributes["src"].Value.Substring(0, 4).ToLower().Equals("http") && 
+					!imageNode.Attributes["src"].Value.Substring(0, 2).ToLower().Equals("//") && 
+					!imageNode.Attributes["src"].Value.Substring(0, 4).ToLower().Equals("data") )
+				{
+					imageNode.Attributes["src"].Value = "http://" + url + "/" + imageNode.Attributes["src"].Value;
+				}
+			}
+
             return Content(doc.DocumentNode.OuterHtml);
         }
     }
