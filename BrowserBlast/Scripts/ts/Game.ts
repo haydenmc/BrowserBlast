067a69981557127c393e6a342d7196ccf757f5cf
Darren Cheng <zades@outlook.com>
2014-10-19 06:46:27 -0400

    Cleaned up code

--- BrowserBlast/Scripts/ts/Game.ts
+++ BrowserBlast/Scripts/ts/Game.ts
@@ -24,21 +24,16 @@
 
 	public processIFrame(window: Window) {
 		console.dir(window.document);
-		this.document = window.document;
-		var newNode = <HTMLElement>window.document.body.cloneNode(true);
-        window.document.body.parentNode.replaceChild(newNode, window.document.body);
-        var list: Array<string> = ["a", "img", "p", "h1", "h2","h3", "button", "input", "li"];
-        for (var j = 0; j < 8; j++)
+
+        var list: Array<string> = ["a", "img", "p", "h1", "h2","h3", "button", "input", "li", "label"];
+        for (var j = 0; j < list.length; j++)
         {
             var listOfLinks = window.document.getElementsByTagName(list[j]);
             console.dir(listOfLinks);
             for (var i = 0; i < listOfLinks.length; i++) {
                 ((link: Node) => {
-                    //link.attributes["href"] = "";
                     (<HTMLElement>link).setAttribute("href", "javascript:;");
                     (<HTMLElement>link).setAttribute("target", "");
-                    //var newLink = <HTMLElement> link.cloneNode(true);
-                    //newLink.parentNode.replaceChild(newLink, link);
 
                     (<HTMLElement>link).addEventListener("click", () => { this.killElement(<HTMLElement>link); }, false);
                 })(listOfLinks[i]);
